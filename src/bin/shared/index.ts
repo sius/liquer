04acaf1dcd949dcc14e27e592c7de79754521fd6
sius <sius@gmx.net>
2019-01-15 01:47:03 +0100

    unused files

--- src/bin/shared/index.ts
+++
@@ -1,3 +0,0 @@
-export const MAVEN_OPTIONS_GROUP = `Maven Options:`;
-export const AUDIT_OPTIONS_GROUP = `Maven Audit Options:`;
-export const REPORT_OPTIONS_GROUP = `Maven Audit Options:`;
--- src/lib/dependency.ts
+++
@@ -1,4 +0,0 @@
-export interface Dependency {
-  _id?: string;
-  tool: string;
-}
